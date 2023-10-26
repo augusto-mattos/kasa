bc7eadb0f939360587941dd3b2268815d8ed4a5d
augusto-mattos <guto.s.mattos@gmail.com>
2023-10-26 18:20:08 +0200

    add accordion

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,12 @@
 import Banner from "../../components/banner";
+import Collapse from "../../components/collapse";
 
 function About() {
   return (
+    <>
       <Banner />
+      <Collapse />
+    </>
   );
 }
 
