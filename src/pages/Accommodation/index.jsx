df200ea2ecc7eeec01726a7e1b56fdbff5241f20
augusto-mattos <guto.s.mattos@gmail.com>
2023-10-31 14:12:33 +0100

    refactor: include props in the carrousel component

--- src/pages/Accommodation/index.jsx
+++ src/pages/Accommodation/index.jsx
@@ -1,9 +1,21 @@
+import { useLocation } from "react-router-dom";
+import data from "../../data/annonces.json";
 import Carrousel from "../../components/carrousel";
 
 function FicheLogement() {
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  const urlLogementId = currentUrl.split("/")[2];
+  const logement = data.find((item) => item.id === urlLogementId);
+
+  const title = logement.title;
+
   return (
-    <Carrousel />
-  )
+    <>
+      <Carrousel logement={logement} />
+      <h1>{title}</h1>
+    </>
+  );
 }
 
 export default FicheLogement;
